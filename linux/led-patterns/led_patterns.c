75fbe5e82240897bcece77964b4f6e9f952cd6d0
dirk <dirk.kaiser10@gmail.com>
2024-11-30 16:36:45 -0700

    lab-11 bash

--- linux/led-patterns/led_patterns.c
+++ linux/led-patterns/led_patterns.c
@@ -13,6 +13,28 @@
 #define LED_REG_OFFSET 0x8
 #define SPAN 16
 
+
+/*
+ * struct led_patterns_dev - Private led patterns device struct.
+ * @base_addr: Pointer to the component's base address
+ * @hps_led_control: Address of the hps_led_control register
+ * @base_period: Address of the base_period register
+ * @led_reg: Address of the led_reg register
+ * @miscdev: miscdevice used to create a character device
+ * @lock: mutex used to prevent concurrent writes to memory
+ * 
+ * An led_patterns_dev struct gets created for each eld patterns componendt.
+ */
+struct led_patterns_dev {
+	void __iomem *base_addr;
+	void __iomem *hps_led_control;
+	void __iomem *base_period;
+	void __iomem *led_reg;
+	struct miscdevice miscdev;
+	struct mutex lock;
+};
+
+
 /*
  * led_reg_show() - Return the led_reg value to user-space via sysfs.
  * @dev: Device structure for the led_patterns component. This
@@ -48,7 +70,7 @@ static ssize_t led_reg_store(struct device *dev, struct device_attribute *attr, 
 {
 	u8 led_reg;
 	int ret;
-	struct led_patterns_dev *priv - dev_get_drvdata(dev);
+	struct led_patterns_dev *priv = dev_get_drvdata(dev);
 
 	// Parse the string we received as a u8
 	// See https://elixir.bootlin.com/linux/latest/source/lib/kstrtox.c#L289
@@ -63,39 +85,113 @@ static ssize_t led_reg_store(struct device *dev, struct device_attribute *attr, 
 	return size;
 }
 
+/**
+ * hps_led_control_show() - Return the hps_led_control value to user-space via sysfs.
+ *
+ * @dev: Device sturcture for the led_Patterns component. THis device struct is embedded in the led_patterns' device struct.
+ * @attr: unused.
+ * @buf: Buffer that gers returned to user-space.
+ *
+ * Return: The number of bytes read.
+ */
+static ssize_t hps_led_control_show(struct device *dev, struct device_attribute *attr, char *buf)
+{
+	bool hps_control;
+
+	// Get the provate led_patterns data out of the dev struct
+	struct led_patterns_dev *priv = dev_get_drvdata(dev);
+
+	hps_control = ioread32(priv->hps_led_control);
+
+	return scnprintf(buf, PAGE_SIZE, "%u\n", hps_control);
+}
+
+/**
+ * hps_led_control_store() - Store the hps_led_control value.
+ * @dev: Device structure for the led_patterns component. 
+ * 	device struct is embbed in the led_patterns' platform device struct.
+ * @attr: unused
+ * @buf: Buffer that contains the hps_led_control value being written
+ * @size: the number of bytes being written.
+ *
+ * Return: The number of bytes stored.
+ */
+static ssize_t hps_led_control_store(struct device *dev, struct device_attribute *attr, const char *buf, size_t size)
+{
+	bool hps_control;
+	int ret;
+	struct led_patterns_dev *priv = dev_get_drvdata(dev);
+
+	ret = kstrtobool(buf, &hps_control);
+	if (ret<0) {
+		//kstrtobool returned an error
+		return ret;
+	}
+
+	iowrite32(hps_control, priv->hps_led_control);
+
+	// Write was successful, so we return the number of bytes we wrote.
+	return size;
+}
+
+/**
+ * base_period_show() - Return the base_period value to user-space via sysfs.
+ * @dev: Device structrue for the led_patterns component. This device struct is embedded in the platfrom struct
+ * @attr: unused again
+ * @buf: buffer that gets returned to user-space.
+ *
+ * Return: the number of bytes read.
+ */
+static ssize_t base_period_show(struct device *dev, struct device_attribute *attr, char *buf)
+{
+	u8 base_period;
+	struct led_patterns_dev *priv = dev_get_drvdata(dev);
+
+	base_period = ioread32(priv->base_period);
+
+	return scnprintf(buf, PAGE_SIZE, "%u\n", base_period);
+}
+
+/**
+ * base_period_store() - Store the base_period value. 
+ * @dev: Device structure fo the led_patterns component. THis device struct is embeedded in the platform struct
+ * @attr: Unsused.
+ * @buf: Buffer that contains the base_period value being written.
+ * @sizeL the number of bytes being written.
+ * 
+ * Return: the number of bytes stored.
+ */
+static ssize_t base_period_store(struct device *dev, struct device_attribute *attr, const char*buf, size_t size)
+{
+	u8 base_period;
+	int ret;
+	struct led_patterns_dev *priv = dev_get_drvdata(dev);
+
+	ret = kstrtou8(buf, 0, &base_period);
+	if (ret<0) {
+		//kstrtou8 returned an error
+		return ret;
+	}
+
+	iowrite32(base_period, priv->base_period);
+
+	// Write was successful, so we return the number of bytes we wrote.
+	return size;
+}
+
 // Define sysfs attributes
 static DEVICE_ATTR_RW(hps_led_control);
 static DEVICE_ATTR_RW(base_period);
 static DEVICE_ATTR_RW(led_reg);
 
 // Create an attribute group so the device core can export the attributes for us.
-static struct attribute *led_patter_attrs[] = {
+static struct attribute *led_patterns_attrs[] = {
 	&dev_attr_hps_led_control.attr,
 	&dev_attr_base_period.attr,
 	&dev_attr_led_reg.attr,
 	NULL,
 };
 ATTRIBUTE_GROUPS(led_patterns);
-
-/*
- * struct led_patterns_dev - Private led patterns device struct.
- * @base_addr: Pointer to the component's base address
- * @hps_led_control: Address of the hps_led_control register
- * @base_period: Address of the base_period register
- * @led_reg: Address of the led_reg register
- * @miscdev: miscdevice used to create a character device
- * @lock: mutex used to prevent concurrent writes to memory
- * 
- * An led_patterns_dev struct gets created for each eld patterns componendt.
- */
-struct led_patterns_dev {
-	void __iomem *base_addr;
-	void __iomem *hps_led_control;
-	void __iomem *base_period;
-	void __iomem *led_reg;
-	struct miscdevice miscdev;
-	struct mutex lock;
-};
 
 /*
  * led_patterns_read() - Read emthod for the led_patterns char device
