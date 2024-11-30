4a5178dfc16dc33f109c3fb72c15e45dd2482684
dirk <dirk.kaiser10@gmail.com>
2024-11-30 13:19:54 -0700

    before doing the sysfs stuff

--- sw/led-patterns/led_patterns_miscdev_test.c
+++ sw/led-patterns/led_patterns_miscdev_test.c
@@ -4,16 +4,71 @@
 #include <errno.h>
 #include <string.h>
 #include <unistd.h>
+#include <signal.h>
 
 // TODO: update these offsets if your address are different
 #define HPS_LED_CONTROL_OFFSET 0x0
 #define BASE_PERIOD_OFFSET 0x4
 #define LED_REG_OFFSET 0x08
 
+FILE *file;
+size_t ret;	
+uint32_t val;
+
+
+void INThandler(int sig)
+{
+	char c;
+
+	signal(sig, SIG_IGN);
+	fprintf(stdout, "\n Are you sure you want to quit? [y/n] \n");
+	c = getchar();
+	if ( c == 'y' || c == 'Y')
+	{
+
+		// Turn on hardware-control mode
+		printf("back to hardware-control mode....\n");
+		val = 0x00;
+		ret = fseek(file, HPS_LED_CONTROL_OFFSET, SEEK_SET);
+		ret = fwrite(&val, 4, 1, file);
+		fflush(file);
+
+		val = 0x12;
+		ret = fseek(file, BASE_PERIOD_OFFSET, SEEK_SET);
+		ret = fwrite(&val, 4, 1, file);
+		fflush(file);
+
+		sleep(5);
+
+		// Speed up the base period!
+		val = 0x02;
+		ret = fseek(file, BASE_PERIOD_OFFSET, SEEK_SET);
+		ret = fwrite(&val, 4, 1, file);
+		fflush(file);
+
+
+		printf("\n************************************\n*");
+		printf("* read new register values\n");
+		printf("************************************\n\n");
+	
+		// Reset file position to 0
+		ret = fseek(file, 0, SEEK_SET);
+
+		ret = fread(&val, 4, 1, file);
+		printf("HPS_LED_control = 0x%x\n", val);
+
+		ret = fread(&val, 4, 1, file);
+		printf("base period = 0x%x\n", val);
+	
+		ret = fread(&val, 4, 1, file);
+		printf("LED_reg = 0x%x\n", val);
+
+		fclose(file);
+		exit(0);
+	}		
+}
+
 int main () {
-	FILE *file;
-	size_t ret;	
-	uint32_t val;
 
 	file = fopen("/dev/led_patterns" , "rb+" );
 	if (file == NULL) {
@@ -46,78 +101,30 @@ int main () {
 	printf("************************************\n\n");
 	// Turn on software-control mode
 	val = 0x01;
-    ret = fseek(file, HPS_LED_CONTROL_OFFSET, SEEK_SET);
+ 	ret = fseek(file, HPS_LED_CONTROL_OFFSET, SEEK_SET);
 	ret = fwrite(&val, 4, 1, file);
 	// We need to "flush" so the OS finishes writing to the file before our code continues.
 	fflush(file);
 
 	// Write some values to the LEDs
 	printf("writing patterns to LEDs....\n");
-	val = 0x55;
-    ret = fseek(file, LED_REG_OFFSET, SEEK_SET);
-	ret = fwrite(&val, 4, 1, file);
-	fflush(file);
 
-	sleep(1);
+	signal(SIGINT, INThandler); // allow for exit with ^C
+	while(1)
+	{
+		val = 0xff;
+		ret = fseek(file, LED_REG_OFFSET, SEEK_SET);
+		ret = fwrite(&val, 4, 1, file);
+		fflush(file);
 
-	val = 0xaa;
-    ret = fseek(file, LED_REG_OFFSET, SEEK_SET);
-	ret = fwrite(&val, 4, 1, file);
-	fflush(file);
+		sleep(1);
 
-	sleep(1);
+		val = 0x00;
+		ret = fseek(file, LED_REG_OFFSET, SEEK_SET);
+		ret = fwrite(&val, 4, 1, file);
+		fflush(file);
 
-	val = 0xff;
-    ret = fseek(file, LED_REG_OFFSET, SEEK_SET);
-	ret = fwrite(&val, 4, 1, file);
-	fflush(file);
-
-	usleep(0.5e6);
-
-	val = 0x00;
-    ret = fseek(file, LED_REG_OFFSET, SEEK_SET);
-	ret = fwrite(&val, 4, 1, file);
-	fflush(file);
-
-	sleep(1);
-
-	// Turn on hardware-control mode
-	printf("back to hardware-control mode....\n");
-	val = 0x00;
-    ret = fseek(file, HPS_LED_CONTROL_OFFSET, SEEK_SET);
-	ret = fwrite(&val, 4, 1, file);
-	fflush(file);
-
-	val = 0x12;
-    ret = fseek(file, BASE_PERIOD_OFFSET, SEEK_SET);
-	ret = fwrite(&val, 4, 1, file);
-	fflush(file);
-
-	sleep(5);
-
-	// Speed up the base period!
-	val = 0x02;
-    ret = fseek(file, BASE_PERIOD_OFFSET, SEEK_SET);
-	ret = fwrite(&val, 4, 1, file);
-	fflush(file);
-
-
-	printf("\n************************************\n*");
-	printf("* read new register values\n");
-	printf("************************************\n\n");
-	
-	// Reset file position to 0
-	ret = fseek(file, 0, SEEK_SET);
-
-	ret = fread(&val, 4, 1, file);
-	printf("HPS_LED_control = 0x%x\n", val);
-
-	ret = fread(&val, 4, 1, file);
-	printf("base period = 0x%x\n", val);
-
-	ret = fread(&val, 4, 1, file);
-	printf("LED_reg = 0x%x\n", val);
-
-	fclose(file);
+		sleep(1);
+	}
 	return 0;
 }
